306cce21ab6dbed47f0f341e03859195b2b3c5b4
Hans Kevin Villacampa <vhanskevin@gmail.com>
2020-08-25 01:38:16 +0800

    Initial commit to coming soon app"

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ComingSoon from "react-coming-soon";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <ComingSoon
+        title='Launching Soon'
+        subtitle="when I'm done being lazy!"
+        bgColor='#ffffff'
+        textColor='#212121'
+        illustration='git'
+      />
     </div>
   );
 }
